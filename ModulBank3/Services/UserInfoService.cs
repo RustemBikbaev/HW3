d75aa5912babc7e874d1b9a9b0f28359070d12dc
Rustem bikbaev <ibikbaev97@gmail.ru>
2019-04-28 20:10:38 +0500

    2/4

--- ModulBank3/Services/UserInfoService.cs
+++ ModulBank3/Services/UserInfoService.cs
@@ -12,14 +12,28 @@ namespace ModulBank3.Services
     public class UserInfoService : IUserInfoService
     {
         private const string ConnectionString =
-            "host=localhost;port=5432;database=postgres;username=postgres;password=1";
+            "host=localhost;port=5432;database=homework3;username=postgres;password=1";
+
+        //public async Task<User> GetById(Guid id)
+        //{
+        //    User user = new User
+        //    {
+        //        Email = "test@test.ru",
+        //        Id = id,
+        //        Nickname = "test",
+        //        Phone = "+7 987 654 32 10"
+        //    };
+
+        //    return await Task.FromResult<User>(user);
+        //}
+
 
         public async Task<User> GetById(Guid id)
         {
             using (var connection = new NpgsqlConnection(ConnectionString))
             {
                 return await connection.QuerySingleAsync<User>(
-                    "SELECT * FROM Users WHERE Id = @id", new { id });
+                    "SELECT * FROM users WHERE Id = @id", new { id });
             }
         }
     }
